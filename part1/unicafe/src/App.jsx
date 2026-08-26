34a14d7062d9b234d1bed8a64a4856d0552acd6c
Felix Alexandre <felixnovela104@gmail.com>
2026-08-26 23:05:33 +0200

    Unicafe step 6: display statistics in HTML table

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,9 +11,10 @@ const Button = ({text, onClick}) => {
 }
 const StatisticLine = (props) =>{
     return(
-        <p>
-            {props.text} {props.value}
-        </p>
+        <tr>
+            <td>{props.text}</td>
+            <td>{props.value}</td>
+        </tr>
     )
 }
 const Statistics = (props) => {
@@ -29,12 +30,17 @@ const Statistics = (props) => {
     return(
         <>
             <h2>statistics</h2>
-            <StatisticLine text="good" value={props.good} />
-            <StatisticLine text="neutral" value={props.neutral} />
-            <StatisticLine text="bad" value={props.bad} />
-            <StatisticLine text="all" value={props.statistics.total} />
-            <StatisticLine text="average" value={props.statistics.average} />
-            <StatisticLine text="positive" value={props.statistics.positive+"%"} />
+            <table>
+                <tbody>
+                    <StatisticLine text="good" value={props.good} />
+                    <StatisticLine text="neutral" value={props.neutral} />
+                    <StatisticLine text="bad" value={props.bad} />
+                    <StatisticLine text="all" value={props.statistics.total} />
+                    <StatisticLine text="average" value={props.statistics.average} />
+                    <StatisticLine text="positive" value={props.statistics.positive+"%"} />
+                </tbody>
+            </table>
+            
             
         </>
     )
